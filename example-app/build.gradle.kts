149dfb8ceaf82036e9b1ff2465e1127a4492ab65
Ricardo Gonçalves <ricardobrg@users.noreply.github.com>
2024-04-16 16:14:47 -0300

    refactor: move example out of client

--- example-app/build.gradle.kts
+++ example-app/build.gradle.kts
@@ -51,7 +51,6 @@ android {
 }
 
 dependencies {
-    //Jetpack Compose
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
@@ -65,7 +64,9 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
     implementation(project(":client"))
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
